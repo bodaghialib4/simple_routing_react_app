8d576629278f7327cdd520b6961b311922fdf19b
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-12 17:21:05 +0330

    implement Header.js component and use it in App.j (Link path not set)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import Footer from "./layouts/Footer";
+import Header from "./layouts/Header";
 
 import Home from './components/Home'
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={Home}/>
       </Switch>
---
+++ src/layouts/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div
+      className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h5 className="my-0 mr-md-auto font-weight-normal">
+        <Link to="/" className="p-2 text-dark">Remote tools</Link>
+      </h5>
+      <nav className="my-2 my-md-0 mr-md-3">
+        <Link to="/" className="p-2 text-dark">Collections</Link>
+        <Link to="/" className="p-2 text-dark">About</Link>
+      </nav>
+      <Link to="/" className="btn btn-outline-primary">Login</Link>
+    </div>
+  );
+};
+
+export default Header;
