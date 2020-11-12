d8594af43e8026cd31a5e1714574043cccd719fb
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-12 14:20:18 +0330

    implement Home component (Link path not set)

---
+++ src/components/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+import slack from './../assets/slack.png'
+import twist from './../assets/twist.png'
+import paymo from './../assets/paymo.png'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+
+        <div className="col-md-3">
+          <div className="card mb-4 shadow-sm">
+            <img className="card-img-top" src={slack} alt='slack'/>
+            <div className="card-body">
+              <p className="card-text">Slack is a new-age team communication and collaboration tool.</p>
+              <Link to={"/"} className="btn btn-sm btn-block btn-outline-secondary">View</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-3">
+          <div className="card mb-4 shadow-sm">
+            <img className="card-img-top" src={twist} alt='twist'/>
+            <div className="card-body">
+              <p className="card-text">Teams have hundreds of conversations happening at once </p>
+              <Link to={"/"} className="btn btn-sm btn-block btn-outline-secondary">View</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-3">
+          <div className="card mb-4 shadow-sm">
+            <img className="card-img-top" src={paymo} alt='paymo'/>
+            <div className="card-body">
+              <p className="card-text">Paymo acts as a single source of truth when it comes to managing...</p>
+              <Link to={"/"} className="btn btn-sm btn-block btn-outline-secondary">View</Link>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
